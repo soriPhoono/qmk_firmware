71a1b01055db560c56554cabb1d4c410b44a0e8b
soriphoono <soriphoono@gmail.com>
2024-10-28 12:33:47 -0500

    Setup direnv

--- flake.nix
+++ flake.nix
@@ -9,8 +9,13 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells.default = import ./shell.nix;
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            nixpkgs-fmt
+          ];
+        };
       };
     };
 }
