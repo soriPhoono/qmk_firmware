77e286087da8212f1ad82e3a9c9249b7e4f4606a
soriphoono <soriphoono@gmail.com>
2024-10-28 12:57:23 -0500

    Updated devshell

--- flake.nix
+++ flake.nix
@@ -10,12 +10,50 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixd
-            nixpkgs-fmt
-          ];
-        };
+        devShells.default =
+          let
+            avrlibc = pkgs.pkgsCross.avr.libcCross;
+
+            avr_incflags = [
+              "-isystem ${avrlibc}/avr/include"
+              "-B${avrlibc}/avr/lib/avr5"
+              "-L${avrlibc}/avr/lib/avr5"
+              "-B${avrlibc}/avr/lib/avr35"
+              "-L${avrlibc}/avr/lib/avr35"
+              "-B${avrlibc}/avr/lib/avr51"
+              "-L${avrlibc}/avr/lib/avr51"
+            ];
+          in
+          pkgs.mkShell {
+            name = "qmk-firmware";
+
+            packages = with pkgs; [
+              nixd
+              nixpkgs-fmt
+
+              clang-tools
+              dfu-programmer
+              dfu-util
+
+              pkgsCross.avr.buildPackages.binutils
+              pkgsCross.avr.buildPackages.gcc8
+              avrlibc
+              avrdude
+
+              gcc-arm-embedded
+
+              teensy-loader-cli
+
+              qmk
+            ];
+
+            AVR_CFLAGS = avr_incflags;
+            AVR_ASFLAGS = avr_incflags;
+
+            shellHook = ''
+              unset NIX_CFLAGS_COMPILE_FOR_TARGET
+            '';
+          };
       };
     };
 }
