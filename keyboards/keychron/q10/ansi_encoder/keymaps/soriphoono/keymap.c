48925f0777ae72fc21eb75fc37f403713b076d17
soriphoono <soriphoono@gmail.com>
2024-10-30 10:34:10 -0500

    Created basic function for rgb formula

--- keyboards/keychron/q10/ansi_encoder/keymaps/soriphoono/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/soriphoono/keymap.c
@@ -74,12 +74,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 #if defined (RGB_MATRIX_ENABLE)
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (host_keyboard_led_state().caps_lock) {
-        for (uint8_t i = led_min; i < led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, RGB_RED);
-            }
-        }
+    if (rgb_matrix_is_enabled()) {
+        // Set Background to cyan
+        rgb_matrix_set_color_all(RGB_CYAN);
     }
 
     return false;
